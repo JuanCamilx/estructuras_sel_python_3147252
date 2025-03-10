a7dca6c740c1fe3d185297b07565b38124dbb4eb
Andres Meza <afmeza51@misena.edu.co>
2025-03-10 09:51:02 -1200

    operador or

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -9,3 +9,12 @@ Acorde a las tablas de verdad
 #Ejemplo 1: Operador not:
 y = not True 
 print("El resultado de operar con not es" ,y)
+
+#ejemplo 2: Operador and
+y = False and True
+print("El resultado de operar con and es" ,y)
+
+
+#ejemplo 3: operador or 
+y = False or True
+print("El resultado de operar con or es" ,y)
